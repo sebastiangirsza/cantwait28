3f544766f896c57205c7001689901e0820a94ed3
Sebastian Girsza <sebag96@wp.pl>
2022-09-02 20:57:15 +0200

    Implement item Model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel({
+    required this.id,
+    required this.title,
+    required this.imageURL,
+    required this.releseDate,
+  });
+  final String id;
+  final String title;
+  final String imageURL;
+  final DateTime releseDate;
+}
