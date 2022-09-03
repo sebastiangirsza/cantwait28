9762d8335c18c58a4372edc1e463c31d99221708
Sebastian Girsza <sebag96@wp.pl>
2022-09-03 17:14:58 +0200

    Add Details page

---
+++ lib/features/details/cubit/details_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:cantwait28/models/item_model.dart';
+import 'package:cantwait28/repositories/items_repository.dart';
+import 'package:meta/meta.dart';
+
+part 'details_state.dart';
+
+class DetailsCubit extends Cubit<DetailsState> {
+  DetailsCubit(this._itemsRpository) : super(DetailsState(itemModel: null));
+
+  final ItemsRpository _itemsRpository;
+
+  Future<void> getItemWithID(String id) async {
+    final itemModel = await _itemsRpository.get(id: id);
+    emit(DetailsState(itemModel: itemModel));
+  }
+}
---
+++ lib/features/details/cubit/details_state.dart
@@ -0,0 +1,8 @@
+part of 'details_cubit.dart';
+
+@immutable
+class DetailsState {
+  final ItemModel? itemModel;
+
+  DetailsState({required this.itemModel});
+}
